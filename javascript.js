018dd1de61622596497111559a1a72de37469762
Persephone <134444980+PersephoneXO@users.noreply.github.com>
2023-11-22 14:37:26 -0500

    basic 16x16 grid created

---
+++ javascript.js
@@ -0,0 +1,71 @@
+/*const gridContainer=document.querySelector('.grid-container');
+
+function createGrid(num){
+    const fragment=document.createDocumentFragment();
+    document.documentElement.style.setProperty('--gridAmount',num);
+    for(let i=0;i<num;i++){
+        const row=document.createElement('div');
+        fragment.appendChild(row);
+
+        for (let j=0;j<num;j++){
+            const square=document.createElement('div');
+            square.classList.add('square');
+            square.style.width=`${500/num}px`;
+            square.style.height=`${500/num}px`;
+            row.appendChild(square);
+        }
+    }
+
+    gridContainer.appendChild(fragment);
+}
+
+function getSize(){
+    gridAmount=prompt('Enter a grid size between 16 & 100: ',16);
+    while(Number(gridAmount)>100 || Number(gridAmount)<=0){
+        gridAmount=prompt("Invalid input! Please enter a grid size between 16 & 100: ");
+    }
+    createGrid(gridAmount);
+}
+
+
+//getSize();
+document.addEventListener('DOMContentLoaded',()=>{
+    getSize();
+});*/
+
+
+/*2nd attempt at the js*/
+
+let gridContainer=document.getElementById('grid-container');
+
+function createGrid(size){
+    let squareSize=500/size;
+
+    for(let i=0;i<size*size;i++){
+        let square=document.createElement('div');
+        square.classList.add('square');
+        square.style.width=squareSize+'px';
+        square.style.height=squareSize+'px';
+        gridContainer.appendChild(square);
+    }
+}
+
+//come back to this later
+/*
+function getSize(){
+    gridAmount=prompt('Enter a grid size between 16 & 100: ',16);
+    while(Number(gridAmount)>100 || Number(gridAmount)<=0){
+        gridAmount=prompt("Invalid input! Please enter a grid size between 16 & 100: ");
+    }
+    createGrid(gridAmount);
+}
+
+
+//getSize();
+document.addEventListener('DOMContentLoaded',()=>{
+    getSize();
+});*/
+
+
+let selectedSize=16;
+createGrid(selectedSize);
